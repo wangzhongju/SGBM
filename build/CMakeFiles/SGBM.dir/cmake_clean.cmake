0fbcbc5af32927ff074fdcf5479fe6385f1b3ac9
wangzhongju <wangzhongju@goldenridge.ltd>
2019-12-03 16:56:26 +0800

    SGBM:双目立体视觉匹配

---
+++ build/CMakeFiles/SGBM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SGBM.dir/src/main.cpp.o"
+  "SGBM.pdb"
+  "SGBM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SGBM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
